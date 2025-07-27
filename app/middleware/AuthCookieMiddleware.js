0e3fc64a10d639ad9578d80c1be73b167b0dfab1
Al-Amin Sarker <cseal.amin.09@gmail.com>
2025-07-27 16:42:26 +0600

    feat: cookie based auth middleware for web routes

---
+++ app/middleware/AuthCookieMiddleware.js
@@ -0,0 +1,34 @@
+const { validateToken, verifyToken } =  require('../../helpers/utilities');
+
+class AuthCookieMiddleware {
+	parseCookies = (cookieHeader = '') => {
+	    return Object.fromEntries(
+	      cookieHeader.split(';').map(c => c.trim().split('='))
+	    );
+  	}
+
+	handle = async (req, res, next) => {
+		try {
+		    const cookies = this.parseCookies(req.headers.cookie || '');
+
+		    const token = cookies['session_token'];
+		    const validToken = validateToken(token);
+			const tokenData = await verifyToken(token);
+
+			if (token && validToken && tokenData) {
+		     	next();
+			} else {
+		      	res.writeHead(302, { Location: '/home' });
+		     	res.end();
+		    }
+		} catch (err) {
+			res.writeHead(500, { 'Content-Type': 'application/json' });
+
+			res.end(JSON.stringify({ message: 'Internal Server Error' }));
+		}
+  	}
+}
+
+const middleware = new AuthCookieMiddleware();
+
+module.exports = middleware.handle;
--- routes/web.js
+++ routes/web.js
@@ -1,15 +1,15 @@
 const Route = require('../system/WebRoute');
 
-const AuthMiddleware = require('../app/middleware/AuthMiddleware');
+const AuthCookieMiddleware = require('../app/middleware/AuthCookieMiddleware');
 const HomeController = require('../app/controllers/HomeController');
 const UserController = require('../app/controllers/UserController');
 const AuthController = require('../app/controllers/AuthController');
 
 Route.get('/home', HomeController.index);
 Route.get('/register', HomeController.register);
-Route.get('/profile', HomeController.profile);
+Route.get('/profile', HomeController.profile, [AuthCookieMiddleware]);
 
 Route.post('/login', AuthController.create);
-Route.post('/logout', AuthController.logout, [AuthMiddleware]);
+Route.post('/logout', AuthController.logout, [AuthCookieMiddleware]);
 
 module.exports = Route;
