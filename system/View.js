2b8ecd41ce04406926b3896071c70d07b2b2f5b0
Al-Amin Sarker <cseal.amin.09@gmail.com>
2025-07-28 15:07:04 +0600

    feat: implement view layout system

--- system/View.js
+++ system/View.js
@@ -2,33 +2,51 @@ const fs = require('fs');
 const path = require('path');
 
 class View {
-	constructor(viewName, data = {}) {
+	constructor(viewName, data = {}, layout = 'layouts/main') {
 		this.viewName = viewName;
 		this.data = data;
+		this.layout = layout;
 	}
 
-	async render() {
-		const filePath = path.join(__dirname, '../views', this.viewName + '.html');
-
+	async renderFile(filePath) {
 		return new Promise((resolve, reject) => {
 			fs.readFile(filePath, 'utf8', (error, content) => {
-				if (error) 
-					return reject('View file not found: ' + this.viewName);
-
-				// Replace {{ key }} with values
-				Object.keys(this.data).forEach(key => {
-					const pattern = new RegExp(`{{\\s*${key}\\s*}}`, 'g');
-
-					content = content.replace(pattern, this.data[key]);
-				});
+				if (error) return reject(`View file not found: ${filePath}`);
 
 				resolve(content);
 			});
 		});
 	}
 
-	static async make(viewName, data = {}) {
-		const view = new View(viewName, data);
+	replacePlaceholders(content, data) {
+		Object.keys(data).forEach(key => {
+			// Replace {{ key }} with values
+			const pattern = new RegExp(`{{\\s*${key}\\s*}}`, 'g');
+
+			content = content.replace(pattern, data[key]);
+		});
+
+		return content;
+	}
+
+	async render() {
+		const viewPath = path.join(__dirname, '../views', this.viewName + '.html');
+		const layoutPath = path.join(__dirname, '../views', this.layout + '.html');
+
+		const viewContent = await this.renderFile(viewPath);
+		const layoutContent = await this.renderFile(layoutPath);
+
+		// First replace {{ content }} in layout with the actual view HTML
+		let finalHtml = layoutContent.replace('{{ content }}', viewContent);
+
+		// Then replace other data placeholders
+		finalHtml = this.replacePlaceholders(finalHtml, this.data);
+
+		return finalHtml;
+	}
+
+	static async make(viewName, data = {},  layout = 'layouts/main') {
+		const view = new View(viewName, data, layout);
 
 		return await view.render();
 	}
